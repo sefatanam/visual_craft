bfafbc68142679635a804ec4ef7bb182973439c1
sefatanam <sefatanam@gmail.com>
2024-06-10 22:00:41 +0600

    complete canvas generative art

--- generative-art-js/sketch.js
+++ generative-art-js/sketch.js
@@ -16,13 +16,14 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        const radius = Math.abs(random.noise2D(u, v)) * 0.05;
+        const radius = Math.abs(random.noise2D(u, v)) * 0.09;
 
         let data = {
           u,
           v,
           color: random.pick(pallete),
           radius,
+          rotation: random.noise2D(u, v),
         };
 
         points.push(data);
@@ -37,18 +38,33 @@ const sketch = () => {
 
   return ({ context, width, height }) => {
     context.fillStyle = random.pick(pallete);
+    // context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
     points.forEach((data) => {
-      const { u, v, color, radius } = data;
+      const { u, v, color, radius, rotation } = data;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
+      // Art
+      /* context.beginPath();
       context.arc(x, y, radius * width, 0, Math.PI * 2, false);
       context.strokeStyle = color;
       context.lineWidth = "10";
-      context.stroke();
+      context.stroke(); */
+
+      // Text
+      context.save();
+      // context.fillStyle = color;
+
+      context.fillStyle = color;
+      context.font = `${radius * width * 2}px "Helvetica"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+
+      // context.fillText(random.pick(["A", "B", "C", "+"]), x, y);
+      context.fillText(random.pick(["="]), 0, 0);
+      context.restore();
     });
   };
 };
