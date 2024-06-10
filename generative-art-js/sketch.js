89d89f087af8898ebe6e846565dec91437547a0b
sefatanam <sefatanam@gmail.com>
2024-06-10 21:38:30 +0600

    added noise 2d

--- generative-art-js/sketch.js
+++ generative-art-js/sketch.js
@@ -11,16 +11,18 @@ const sketch = () => {
   const pallete = random.pick(palettes);
   const createGrid = () => {
     const points = [];
-    const count = 25;
+    const count = 30;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.05;
+
         let data = {
           u,
           v,
           color: random.pick(pallete),
-          radius: Math.max(0, random.gaussian() * 0.03),
+          radius,
         };
 
         points.push(data);
