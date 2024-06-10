67657975c6a5de99fb38788f67d8c9c833ad5f9c
sefatanam <sefatanam@gmail.com>
2024-06-10 20:47:57 +0600

    implement lerp in generative art

--- generative-art-js/sketch.js
+++ generative-art-js/sketch.js
@@ -1,6 +1,7 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
 const palettes = require("nice-color-palettes");
+const { lerp } = require("canvas-sketch-util/math");
 
 const settings = {
   dimensions: [2048, 2048],
@@ -10,7 +11,7 @@ const sketch = () => {
   const createGrid = () => {
     const pallete = random.pick(palettes);
     const points = [];
-    const count = 12;
+    const count = 5;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
@@ -28,6 +29,7 @@ const sketch = () => {
   };
 
   const points = createGrid();
+  const margin = 400;
 
   return ({ context, width, height }) => {
     context.fillStyle = "white";
@@ -35,13 +37,13 @@ const sketch = () => {
 
     points.forEach((data) => {
       const { u, v, color } = data;
-      const x = u * width;
-      const y = v * height;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 120, 0, Math.PI * 2, false);
-      context.strokeStyle = color;
-      context.lineWidth = "20";
+      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.strokeStyle = "color";
+      context.lineWidth = "10";
       context.stroke();
     });
   };
